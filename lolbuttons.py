95b61edf42a9faf2efdb2200966a6d1501723bc4
Richard Patterson <detobate@gmail.com>
2017-07-11 14:47:35 +0100

    Couch lights via particle.io

--- lolbuttons.py
+++ lolbuttons.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python3
+
 from scapy.all import *
 import pywemo
+import requests
+import ujson
 
-dashes = { '44:65:0d:74:58:31': 'whiskas' }
+dashes = { '44:65:0d:74:58:31': 'whiskas', 'ac:63:be:48:c1:9e': 'durex' }
 
 def findDash(pkts):
     for pkt in pkts:
@@ -10,15 +13,37 @@ def findDash(pkts):
             print("Dash %s was pressed" % dashes[pkt.fields['src']])
             triggerDash(dashes[pkt.fields['src']])
 
+def triggerDash(dashName):
+    # Do case matching on the dashes here
+
+    if dashName == 'whiskas':
+        toggleLights(bridge.Lights)
+    elif dashName == 'durex':
+        toggleCouch()
+
 def toggleLights(lights):
     for light in lights:
         print("Toggling: %s" % lights[light].name)
         lights[light].toggle()
 
-def triggerDash(dashName):
-    # switch case match the dashes here
-    if dashName == 'whiskas':
-        toggleLights(bridge.Lights)
+def toggleCouch():
+    baseurl = 'https://api.spark.io/v1/devices/[REDACTED]/'
+    token = '[REDACTED]'
+    try:
+        response = requests.get(baseurl + "status?access_token=" + token)
+        response = ujson.loads(response.text)
+
+        if response['result'] == "off":
+            #url = baseurl + "on"
+            url = baseurl + "setColour"
+        else:
+            url = baseurl + "off"
+
+        data = {'access_token': token, "args": "#E9967A"}
+        response = requests.post(url, data=data)
+        print("Toggling Couch Lights")
+    except:
+        print("Invalid Response")
 
 def main():
 
